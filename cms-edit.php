421aa105c3c4e4187fc3b140e34b293ae8580a68
DrChhun <Chhunfindsome1@gmail.com>
2023-08-18 23:40:47 +0700

    loop data in edit page

--- cms-edit.php
+++ cms-edit.php
@@ -1,17 +1,26 @@
+<?php 
+    require('database.php');
+
+    $result = mysqli_query($connect, "SELECT * from content");
+?>
+
 <body>
     <a href="/cms">back to index</a>
     <a href="/cms/add-content.php">go to add content page</a>
     <h1 style="text-align: center; padding: 2rem;">CMS edit page</h1>
 
-    <div style="max-width: 800px; margin: auto; text-align: center;">
-        <h2>cms title</h2>
-        <p style="text-align: start;">Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Nemo architecto debitis facilis quaerat alias illum sint, 
-            hic dolorum natus aliquid!
-        </p>
-        <div style="display: flex; flex-direction: column; text-align: start; gap: 15px;">
-            <a style="color: green;" href="#">Edit this content</a>
-            <a style="color: red;" href="#">Delete this content</a>
-        </div>
-    </div>
+    <?php
+        while ($data = mysqli_fetch_array($result)) {
+            if ($data['title'] && $data['description']) {
+                echo    '<div style="max-width: 800px; margin: auto; text-align: center;">';
+                echo    '<h2>'.$data['title'].'</h2>';
+                echo    '<p style="text-align: start;">'.$data['description'].'</p>';
+                echo    '<div style="display: flex; flex-direction: column; text-align: start; gap: 15px;">';
+                echo    '<a style="color: green;" href="#">Edit this content</a>';
+                echo    '<a style="color: red;" href="#">Delete this content</a>';
+                echo    '</div>';
+                echo    '</div>';
+            }
+        }
+    ?>
 </body>
