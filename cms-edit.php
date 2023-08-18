dd75b0dae154252548eee023f8513bbefde093d4
DrChhun <Chhunfindsome1@gmail.com>
2023-08-18 15:25:33 +0700

    add cms-edit page and index page

---
+++ cms-edit.php
@@ -0,0 +1,15 @@
+<body>
+    <h1 style="text-align: center; padding: 2rem;">CMS edit page</h1>
+
+    <div style="max-width: 800px; margin: auto; text-align: center;">
+        <h2>cms title</h2>
+        <p style="text-align: start;">Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Nemo architecto debitis facilis quaerat alias illum sint, 
+            hic dolorum natus aliquid!
+        </p>
+        <div style="display: flex; flex-direction: column; text-align: start; gap: 15px;">
+            <a style="color: green;" href="#">Edit this content</a>
+            <a style="color: red;" href="#">Delete this content</a>
+        </div>
+    </div>
+</body>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+    $indexPage = "All cms content";
+?>
+
+<body>
+    <h1 style="text-align: center; padding: 2rem;">All cms content</h1>
+    <div style="text-align: center; max-width: 800px; margin: auto;">
+        <h2>Tesla just take over neo in the september.</h2>
+        <p style="text-align: start;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Laudantium corporis quam nobis. Amet ratione tempora adipisci 
+            labore molestias sint libero.
+        </p>
+        <div style="text-align: start;">
+            <a href="#">Read more</a>
+        </div>
+    </div>
+</body>
