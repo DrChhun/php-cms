0daa905b797882708cfcd7e9daaf6bd4841c73e7
DrChhun <Chhunfindsome1@gmail.com>
2023-08-21 10:49:26 +0700

    implement styling with tailwind

--- cms-edit.php
+++ cms-edit.php
@@ -1,5 +1,6 @@
 <?php 
     require('database.php');
+    require('link.php');
 
     $result = mysqli_query($connect, "SELECT * from content");
 ?>
@@ -7,19 +8,22 @@
 <body>
     <a href="/cms">back to index</a>
     <a href="/cms/add-content.php">go to add content page</a>
-    <h1 style="text-align: center; padding: 2rem;">CMS edit page</h1>
+    <h1 class="text-center mx-auto text-5xl font-semibold mt-[84px] mb-[48px] text-[#222222] max-w-xl">Start Manage Your Content.</h1>
 
     <?php
         while ($data = mysqli_fetch_array($result)) {
             if ($data['title'] && $data['description']) {
-                echo    '<div style="max-width: 800px; margin: auto; text-align: center;">';
-                echo    '<h2>'.$data['title'].'</h2>';
-                echo    '<p style="text-align: start;">'.$data['description'].'</p>';
-                echo    '<div style="display: flex; flex-direction: column; text-align: start; gap: 15px;">';
-                echo    '<a style="color: green;" href="#">Edit this content</a>';
-                echo    '<a style="color: red;" href="#">Delete this content</a>';
-                echo    '</div>';
-                echo    '</div>';
+                echo '<div class="mx-[266px]">';
+                echo '<h2 class="text-2xl font-semibold mb-2 text-[#222222]">'.$data['title'].'</h2>';
+                echo '<p class="text-[#717171] text-light text-lg">'.$data['description'].'</p>';
+                echo '<div>';
+                echo '</div>';
+                echo '<div class="mt-10 flex gap-6">';
+                echo '<object data="icons/trash.svg" class="!cursor-pointer" width="24" height="24"> </object>';
+                echo '<object data="icons/status.svg" class="!cursor-pointer" width="24" height="24"> </object>';
+                echo '</div>';
+                echo '<hr class="text-[#dddddd] mt-10 mb-6">';
+                echo '</div>';
             }
         }
     ?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     require('database.php');
+    require('link.php');
 
     $result = mysqli_query($connect, "SELECT * from content");
 ?>
@@ -7,16 +8,19 @@
 <body>
     <a href="/cms/cms-edit.php">navigate to edit page</a>
     <a href="/cms/add-content.php">navigate to add content page</a>
-    <h1 style="text-align: center; padding: 2rem;">All cms content</h1>
+    <h1 class="text-5xl font-semibold max-w-xl text-center mx-auto mb-10 mt-[84px] text-[#222222]">Hello welcome to my page this is the blog page that I build by my self.</h1>
+    <h2 class="text-2xl font-light text-[#717171] text-center mx-auto mb-[48px]">This website I build using PHP and Tailwind.</h2>
+    <h2 class="text-xl font-medium text-center mx-auto mb-2">Start Reading</h2>
+    <object data="icons/arrow-down.svg" class="mx-auto mb-[48px]" width="24" height="24"> </object>
     <?php
         while ($data = mysqli_fetch_array($result)) {
             if ($data['title'] && $data['description']) {
-                echo '<div style="text-align: center; max-width: 800px; margin: auto;">';
-                echo "<h2>".$data['title']."</h2>";
-                echo '<p style="text-align: start;">'.$data['description'].'</p>';
-                echo '<div style="text-align: start;">';
-                echo '<a href="#">Read more</a>';
+                echo '<div class="mx-[266px] cursor-pointer">';
+                echo '<h2 class="text-2xl font-semibold mb-2 text-[#222222]">'.$data['title'].'</h2>';
+                echo '<p class="text-[#717171] text-light text-lg">'.$data['description'].'</p>';
+                echo '<div>';
                 echo '</div>';
+                echo '<hr class="text-[#dddddd] mt-10 mb-6">';
                 echo '</div>';
             }
         }
